a889d20d55ff9b5fe967ce5b77addd6fc0b82c95
Tobi Shitta-Bey <tsb99@Tobis-MacBook-Pro-2.local>
2021-02-11 16:04:49 +0000

    old project git cleanup

--- src/App.js
+++ src/App.js
@@ -1,26 +1,107 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+ import React from 'react';
+ import { observer } from 'mobx-react';
+ import UserStore from './stores/UserStores';
+ import LoginForm from './LoginForm';
+ import SubmitButton from './SubmitButton'
+ import RegisterForm from './RegisterForm'
+ import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+
+  async componentDidMount() {
+
+    try {
+
+      let res = await fetch('/isLoggedIn', {
+        method: 'post',
+        headers: {
+          'Accept' : 'application/json',
+          'Content-Type' : 'application/json'
+        }
+      })
+
+      let result = await res.json();
+
+      if(result && result.success){
+        UserStore.loading = false;
+        UserStore.isLoggedIn = true;
+        UserStore.username = result.username;
+      }
+
+      else {
+        UserStore.loading = false;
+        UserStore.isLoggedIn = false;
+      }
+    }
+
+    catch(e) {
+      UserStore.loading = false;
+      UserStore.isLoggedIn = false;
+    }
+
+  }
+
+  async doLogout() {
+
+    try {
+
+      let res = await fetch('/logout', {
+        method: 'post',
+        headers: {
+          'Accept' : 'application/json',
+          'Content-Type' : 'application/json'
+        }
+      });
+
+      let result = await res.json();
+
+      if(result && result.success){
+        UserStore.isLoggedIn = false;
+        UserStore.username = '';
+      }
+    }
+
+    catch(e) {
+      console.log(e)
+    }
+
+  }
+
+  render() {
+    if(UserStore.loading) {
+      return(
+        <div className="app">
+          <div className="container">
+            Loading Page
+          </div>
+        </div>
+      );
+    } else {
+      if(UserStore.isLoggedIn){
+        // redirect to main page with user properties attached
+        return (
+          <div className="app">
+            <div className = 'container'>
+              <a href='http://localhost:3001' > Welcome {UserStore.username} </a>
+              <SubmitButton
+                  text = {'Log out'}
+                  disabled = {false}
+                  onClick = { () => this.doLogout()}
+              />
+            </div>
+          </div>
+        )
+      }
+    }
+      return (
+        <div className="app">
+          <div className="container">
+            <LoginForm />
+          </div>
+        </div>
+      );
+  }
 }
 
-export default App;
+
+export default observer(App);
